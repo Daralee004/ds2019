a662e5362ea1753da9cfbfaf16d68031f6a56eca
daralee004 <daramedia007@gmail.com>
2019-11-01 15:07:33 +0100

    my first work

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+let name ="Dara"
+let num = 12 
+let address ="akinrele street"
+
+console.log (`my name is ${name}, I live at ${num} ${address}`)
+
+
+
+document.write (3*10+20/5)
+
+
+
+
+
+
